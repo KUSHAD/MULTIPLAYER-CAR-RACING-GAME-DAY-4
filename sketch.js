e0ff64a995d742daaba095c6e580f9d91df93434
KUSHAD <kushadck@gmail.com>
2020-04-18 19:44:11 +0530

    ADDED THE TRACK IMAGES AND ALSO FOR THE CARS

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var canvas, backgroundImage;
+var canvas ;
 
 var gameState = 0;
 var playerCount;
@@ -10,6 +10,16 @@ var form, player, game;
 
 var cars, car1, car2, car3, car4;
 
+var car1Img,car2Img,car3Img,car4Img,track,ground;
+
+function preload(){
+  car1Img=loadImage("./images/car1.png");
+  car2Img=loadImage("./images/car2.png");
+  car3Img=loadImage("./images/car3.png");
+  car4Img=loadImage("./images/car4.png");
+  ground=loadImage("./images/ground.png");
+  track=loadImage("./images/track.jpg");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
